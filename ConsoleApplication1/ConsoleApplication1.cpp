7ccbc1eab4c3bd422988270dc8793f202174a9b8
olisss <olisss337@gmail.com>
2023-03-09 20:33:51 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream> 
+using namespace std;
+//1) ЗАДАНИЕ - придумать 3 ситуации с агрегированием
+/*
+Дерево-листик, банка-кола, ящик-ручка
+*/
+
+
+//2) ЗАДАНИЕ -  написать код реализующий АГРЕГИРОВАНИЕ 
+
+// Кола
+class Petal
+{
+    string color; // У колы есть цвет
+public:
+    Petal(string _color) {
+        color = _color;
+    }
+
+    string getColor() {
+        return color;
+    }
+};
+
+// Появилась банка и только потом пявилачь кола
+class Can // Первый тип
+{
+public:
+    Cola* cola;// Реализация АГРЕГИРОВАНИЯ
+
+    void GrowPetal(Cola* _cola)
+    {
+        can = _cola;
+        cout << "Сделали колу нового цвета - " << _cola->getColor() << endl;
+    }
+
+    void PlugPetal()
+    {
+        cola = nullptr;
+        cout << "Колу разлили";
+    }
+};
+
+int main()
+{
+    setlocale(0, "");
+
+    Can* can = new Can;
+
+    Cola* cola = new Cola("Лфйм");
+
+    can->GrowCola(cola);
+
+    can->PlugCola();
+
+    delete cola;
+}
